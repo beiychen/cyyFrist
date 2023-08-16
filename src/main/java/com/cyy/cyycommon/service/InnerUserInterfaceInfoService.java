3a09a44fd988781835e6093aa76c4185ec305ccc
beiychen <15399227250@163.com>
2023-08-16 10:08:45 +0800

    api

---
+++ src/main/java/com/cyy/cyycommon/service/InnerUserInterfaceInfoService.java
@@ -0,0 +1,15 @@
+package com.cyy.cyycommon.service;
+
+/**
+ *
+ */
+public interface InnerUserInterfaceInfoService {
+
+    /**
+     * 调用接口统计
+     * @param interfaceInfoId
+     * @param userId
+     * @return
+     */
+    boolean invokeCount(long interfaceInfoId, long userId);
+}
